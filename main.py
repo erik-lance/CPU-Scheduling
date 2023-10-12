165ce29bff348ff0f553f05e3234e38568b92aee
erik-lance <eriklance@gmail.com>
2023-10-12 22:18:28 +0800

    Added initial input

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # CPU Scheduling Simulator in CLI
-
+from process import Process 
 # First input must contain 3 numbers:
 # [Algorithm, Number of Processes, Quantum]
 
@@ -13,4 +13,17 @@ algorithm = -1
 num_processes = 0
 quantum = 0
 
+processes = []
+queue = []
+
 # Main
+start_input = input().split()
+
+algorithm = int(start_input[0])
+num_processes = int(start_input[1])
+quantum = int(start_input[2])
+
+# Get processes
+for i in range(num_processes):
+    process_input = input().split()
+    processes.append(Process(i, int(process_input[0]), int(process_input[1])))
