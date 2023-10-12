1f3c1b8976518729cf0398fed116ffc6ce89f80e
erik-lance <eriklance@gmail.com>
2023-10-12 22:09:39 +0800

    Added calculations for waiting time

--- process.py
+++ process.py
@@ -5,7 +5,6 @@ class Process:
         self.burst_time = burst_time
         self.remaining_time = burst_time
         self.start_end_time = []
-        self.waiting_time = 0
     
     def add_start_end_time(self, start_time, end_time):
         """Adds a tuple of start and end time to the list of start and end times. Also subtracts the time from the remaining time.
@@ -28,6 +27,35 @@ class Process:
             time_string += f"start time: {start_time} end time: {end_time} | "
         
         return time_string[:-3] # Remove last " | "
+    
+    def get_turnaround_time(self):
+        """Calculates the turnaround time of the process.
+
+        :return: The turnaround time of the process.
+        """
+        
+        # Turnaround time = End time - Arrival time
+        # Get last end time
+        end_time = self.start_end_time[-1][1]
+        return end_time - self.arrival_time
+
+
+    def get_waiting_time(self):
+        """Calculates the waiting time of the process.
+
+        :return: The waiting time of the process.
+        """
+        
+        # Waiting time = Turnaround time - Burst time
+        return self.get_turnaround_time() - self.burst_time
+
+    def get_id(self):
+        """Returns the ID of the process.
+
+        :return: The ID of the process.
+        """
+        
+        return self.id
 
     def __str__(self):
         return f"{self.id} {self.get_start_end_time_string} | Waiting Time: {self.waiting_time}"
