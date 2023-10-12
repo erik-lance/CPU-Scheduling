d68b5a6f0a15b8fb993aa41882e0d22bc1b87846
erik-lance <eriklance@gmail.com>
2023-10-12 22:23:22 +0800

    Added validation for inputs

--- main.py
+++ main.py
@@ -19,11 +19,34 @@ queue = []
 # Main
 start_input = input().split()
 
-algorithm = int(start_input[0])
-num_processes = int(start_input[1])
-quantum = int(start_input[2])
+# Check if valid
+if len(start_input) != 3:
+    print("Invalid input.")
+    exit(1)
+else:
+    algorithm = int(start_input[0])
+    num_processes = int(start_input[1])
+    quantum = int(start_input[2])
+
+# Check if valid
+if algorithm not in range(4):
+    print("Invalid algorithm.")
+    exit(1)
+elif num_processes < 3 and num_processes > 100:
+    print("Invalid number of processes.")
+    exit(1)
+elif quantum < 1 and quantum > 100:
+    print("Invalid quantum.")
+    exit(1)
 
 # Get processes
 for i in range(num_processes):
     process_input = input().split()
-    processes.append(Process(i, int(process_input[0]), int(process_input[1])))
+    if len(process_input) != 3:
+        print("Invalid input.")
+        exit(1)
+    else:
+        processes.append(Process(int(process_input[0]), int(process_input[1]), int(process_input[2])))
+
+
+
