0358193923f72c53738f69e13a464f1becd53f59
erik-lance <eriklance@gmail.com>
2023-10-12 21:30:13 +0800

    Added initial variables

---
+++ main.py
@@ -0,0 +1,16 @@
+# CPU Scheduling Simulator in CLI
+
+# First input must contain 3 numbers:
+# [Algorithm, Number of Processes, Quantum]
+
+#   Algorithms
+#   1 - First-Come-First-Serve (FCFS)
+#   2 - Shortest-Job-First (SJF)
+#   3 - Shortest-Remaining-Time-First (SRTF)
+#   4 - Round-Robin (RR)
+
+algorithm = -1
+num_processes = 0
+quantum = 0
+
+# Main
